3d038b153e1d2d311520626e4cf04798aac8736d
Stremetskiy Mechislav <mechislav.stremetskiy@gmail.com>
2025-01-18 20:54:16 +0300

    sneak 1.1v, player can stop the game

--- js/script.js
+++ js/script.js
@@ -9,29 +9,24 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 const sneak = $(".player")
+const sneakBtn = $(".sneak-btn")
 let sneakY = parseInt(sneak.css("grid-column"))
 let sneakX = parseInt(sneak.css("grid-row"))
 const apple = $(".apple")
 let appleY = parseInt(apple.css("grid-column"))
 let appleX = parseInt(apple.css("grid-row"))
-const start = $(".start")
 let scoreHTML = $(".score span")
 let score = 0
 let gameStop = true
 let data
 
-function gameStoped() {
-  sneakY = 2
-  sneakX = 2
-  apple.hide()
-  gameStop = true
-}
-
 function gameOver() {
+  sneakBtn.text("Start")
   sneakY = 2
   sneakX = 2
   apple.hide()
   gameStop = true
+  data = null
   alert("GameOver")
 }
 
@@ -48,19 +43,14 @@ function move() {
 
 //player moves
 apple.hide()
-start.click(() => {
+sneakBtn.click(() => {
   gameStop = false;
-  start.text('Stop')
+  sneakBtn.text('Stop')
   score = 0
   appleX = Math.floor(Math.random() * 17);
   appleY = Math.floor(Math.random() * 17);
   apple.show()
-  setInterval(() => {
-    console.log("1")
-    if (sneakX <= 0 || sneakX >= 17 || sneakY <= 0 || sneakY >= 17) {
-      gameOver()
-      return
-    }
+  const timerId = setInterval(() => {
     switch (move()) {
       case "w":
         sneakY--
@@ -79,15 +69,15 @@ start.click(() => {
     }
     sneak.css("grid-row", `${sneakY}`)
     sneak.css("grid-column", `${sneakX}`)
-    // start.one("click", () => {
-    //   gameStoped()
-    // })
-    
     if (sneakX == appleX && sneakY == appleY) {
       takeApple()
+    }
+    sneakBtn.click(() => gameStop = true )
+    if (sneakX <= 0 || sneakX >= 17 || sneakY <= 0 || sneakY >= 17 || gameStop == true) {
+      gameOver()
+      clearInterval(timerId)
     }
     apple.css("grid-row", `${appleY}`)
     apple.css("grid-column", `${appleX}`)
   }, 250)
-  return
 })
