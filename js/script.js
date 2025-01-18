61d7a5cacf70a7cd9708e953ee6d13e35aaf7d08
Stremetskiy Mechislav <mechislav.stremetskiy@gmail.com>
2025-01-18 19:53:51 +0300

    sneak 1.0v, sneak can move on her own

--- js/script.js
+++ js/script.js
@@ -1,46 +1,67 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 });
 
 const sneak = $(".player")
 let sneakY = parseInt(sneak.css("grid-column"))
 let sneakX = parseInt(sneak.css("grid-row"))
-let sneakWidth = parseInt(sneak.css("width"))
 const apple = $(".apple")
 let appleY = parseInt(apple.css("grid-column"))
 let appleX = parseInt(apple.css("grid-row"))
 const start = $(".start")
 let scoreHTML = $(".score span")
 let score = 0
+let gameStop = true
+let data
 
-async function gameOver() {
+function gameStoped() {
   sneakY = 2
   sneakX = 2
   apple.hide()
+  gameStop = true
+}
+
+function gameOver() {
+  sneakY = 2
+  sneakX = 2
+  apple.hide()
+  gameStop = true
   alert("GameOver")
 }
 
-async function takeApple() {
+function takeApple() {
   appleX = Math.floor(Math.random() * 17);
   appleY = Math.floor(Math.random() * 17);
   scoreHTML.text(score++)
 }
 
+function move() {
+  $(document).one("keydown", (event) => { data = event.key });
+  return data
+}
 
 //player moves
 apple.hide()
 start.click(() => {
+  gameStop = false;
+  start.text('Stop')
+  score = 0
   appleX = Math.floor(Math.random() * 17);
   appleY = Math.floor(Math.random() * 17);
   apple.show()
-  $(window).keydown(async (event) => {
-    switch (event.key) {
+  setInterval(() => {
+    console.log("1")
+    if (sneakX <= 0 || sneakX >= 17 || sneakY <= 0 || sneakY >= 17) {
+      gameOver()
+      return
+    }
+    switch (move()) {
       case "w":
         sneakY--
         break;
@@ -51,25 +72,22 @@ start.click(() => {
         sneakY++
         break;
       case "d":
-          setInterval(() => { sneakX++; }, 1000)
+        sneakX++
+        break;
+      default:
         break;
     }
-     await sneak.css("grid-row", `${sneakY}`)
-     await sneak.css("grid-column", `${sneakX}`)
-    if (sneakX <= 0 || sneakX >= 17 || sneakY <= 0 || sneakY >= 17) {
-      gameOver()
-    }
+    sneak.css("grid-row", `${sneakY}`)
+    sneak.css("grid-column", `${sneakX}`)
+    // start.one("click", () => {
+    //   gameStoped()
+    // })
+    
     if (sneakX == appleX && sneakY == appleY) {
       takeApple()
     }
     apple.css("grid-row", `${appleY}`)
     apple.css("grid-column", `${appleX}`)
-    sneak.css("width", `${sneakWidth}`)
-  })
+  }, 250)
+  return
 })
-
-
-
-
-
-
