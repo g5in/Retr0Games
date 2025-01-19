615ce15717533424c4284f1413f5fcd8c74dabf3
Stremetskiy Mechislav <mechislav.stremetskiy@gmail.com>
2025-01-19 09:51:16 +0300

    sneak 1.2v, player cant stop the game, but can lose

--- js/script.js
+++ js/script.js
@@ -17,23 +17,14 @@ let appleY = parseInt(apple.css("grid-column"))
 let appleX = parseInt(apple.css("grid-row"))
 let scoreHTML = $(".score span")
 let score = 0
-let gameStop = true
-let data
-
-function gameOver() {
-  sneakBtn.text("Start")
-  sneakY = 2
-  sneakX = 2
-  apple.hide()
-  gameStop = true
-  data = null
-  alert("GameOver")
-}
+let gameStop = false
+let data = "d"
 
 function takeApple() {
   appleX = Math.floor(Math.random() * 17);
   appleY = Math.floor(Math.random() * 17);
-  scoreHTML.text(score++)
+  score++
+  scoreHTML.text(`${score}`)
 }
 
 function move() {
@@ -44,9 +35,7 @@ function move() {
 //player moves
 apple.hide()
 sneakBtn.click(() => {
-  gameStop = false;
   sneakBtn.text('Stop')
-  score = 0
   appleX = Math.floor(Math.random() * 17);
   appleY = Math.floor(Math.random() * 17);
   apple.show()
@@ -72,10 +61,17 @@ sneakBtn.click(() => {
     if (sneakX == appleX && sneakY == appleY) {
       takeApple()
     }
-    sneakBtn.click(() => gameStop = true )
+    // sneakBtn.click(() => gameStop = true )
     if (sneakX <= 0 || sneakX >= 17 || sneakY <= 0 || sneakY >= 17 || gameStop == true) {
-      gameOver()
-      clearInterval(timerId)
+      score = 0
+      sneakY = 2
+      sneakX = 2
+      sneakBtn.text("Start")
+      apple.hide()
+      gameStop = false
+      data = "d"
+      alert("GameOver")
+      setTimeout(() => clearInterval(timerId), 0)
     }
     apple.css("grid-row", `${appleY}`)
     apple.css("grid-column", `${appleX}`)
