c570e0e060e2decb3bcec3c0656b372dd6bf5bf8
g5in(Stremetskiy Mechislav <mechislav.stremetskiy@gmail.com>
2025-01-25 13:51:05 +0300

    2.0v add sneak tail, sneak can't move back

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ const sneakS = $(".sneak-s")
 const sneakD = $(".sneak-d")
 let sneakY = parseInt(sneak.css("grid-column"))
 let sneakX = parseInt(sneak.css("grid-row"))
+let sneakSteps = [];
+let sneakCells = [sneak]
+let i = 0
 const apple = $(".apple")
 let appleY = parseInt(apple.css("grid-column"))
 let appleX = parseInt(apple.css("grid-row"))
@@ -29,15 +32,25 @@ function takeApple() {
   appleY = Math.floor(Math.random() * 17);
   score++
   scoreHTML.text(`${score}`)
+  sneakTail()
 }
 
 function move() {
-  sneakW.one("click", () => data = "w");
-  sneakA.one("click", () => data = "a");
-  sneakS.one("click", () => data = "s");
-  sneakD.one("click", () => data = "d");
-  $(document).one("keydown", (event) => data = event.key);
+  sneakW.one("click", () => data != "s" ? data = "w" : data = data);
+  sneakA.one("click", () => data != "d" ? data = "a" : data = data);
+  sneakS.one("click", () => data != "w" ? data = "s" : data = data);
+  sneakD.one("click", () => data != "a" ? data = "d" : data = data);
+  $(document).one("keydown", (event) => {
+    if (event.key == "a" && data != "d" || event.key == "d" && data != "a" || event.key == "w" && data != "s" || event.key == "s" && data != "w") data = event.key
+    else data = data
+  });
   return data
+}
+
+function sneakTail() {
+  sneakCells.push($('<div class = "player-tail"></div>').appendTo($(".sneak-place")))
+  sneakCells[score].css("grid-column", `${sneakSteps[sneakSteps.length - score - 1].x}`)
+  sneakCells[score].css("grid-row", `${sneakSteps[sneakSteps.length - score - 1].y}`);
 }
 
 //player moves
@@ -64,21 +77,31 @@ sneakBtn.click(() => {
       default:
         break;
     }
-    sneak.css("grid-row", `${sneakY}`)
-    sneak.css("grid-column", `${sneakX}`)
+    sneakSteps.push({ 'x': sneakX, 'y': sneakY })
+    sneakCells.forEach((element) => {
+      element.css("grid-column", `${sneakSteps[sneakSteps.length - i - 1].x}`)
+      element.css("grid-row", `${sneakSteps[sneakSteps.length - i - 1].y}`)
+      i++
+    })
+    i = 0
+    if (sneakSteps.length > score + 2) sneakSteps.shift();
+    console.log(sneakCells)
     if (sneakX == appleX && sneakY == appleY) {
       takeApple()
     }
-    // sneakBtn.click(() => gameStop = true )
+    // sneakBtn.one("click", () => gameStop = true )
     if (sneakX <= 0 || sneakX >= 17 || sneakY <= 0 || sneakY >= 17 || gameStop == true) {
       sneakY = 2
-      sneakX = 2
+      sneakX = 1
       score = 0
       scoreHTML.text(`${score}`)
       sneakBtn.text("Start")
       apple.hide()
       gameStop = false
       data = "d"
+      $(".player-tail").remove()
+      sneakSteps = [];
+      sneakCells = [sneak]
       alert("GameOver")
       setTimeout(() => clearInterval(timerId), 10)
     }
